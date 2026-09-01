19e2b54ccf300545d779a5ef0478ea0fa6da7ae0
anavitoriauribe <anavitoriauribe@gmail.com>
2026-08-31 23:09:13 -0300

    Change line endings in IMC output
    Updated line endings in IMC output to use '/n'.

--- PHPVanilla/4-Funcoes/exercicios/ex01.php
+++ PHPVanilla/4-Funcoes/exercicios/ex01.php
@@ -13,6 +13,6 @@ $imc1 = calcularIMC(60, 1.65);
 $imc2 = calcularIMC(70, 1.70);
 $imc3 = calcularIMC(80, 1.75);
 
-echo "IMC 1: " . number_format($imc1, 2) . PHP_EOL;
-echo "IMC 2: " . number_format($imc2, 2) . PHP_EOL;
-echo "IMC 3: " . number_format($imc3, 2) . PHP_EOL;
+echo "IMC 1: " . number_format($imc1, 2) . "/n";
+echo "IMC 2: " . number_format($imc2, 2) . "/n";
+echo "IMC 3: " . number_format($imc3, 2) . "/n";
